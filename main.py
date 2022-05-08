96406bccb00dc8e245ea8e6ebd9e7e53d3fe7fe6
zxasqwcdevf <zxasqwcdevf@gmail.com>
2022-05-09 08:44:16 +0900

    commit

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-from datetime import datetime, time, timedelta
-from typing import Optional
-from uuid import UUID
-import uuid
-
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.put("/items/{item_id}")
-async def read_items(
-    item_id: UUID,
-    start_datetime: Optional[datetime] = Body(None),
-    end_datetime: Optional[datetime] = Body(None),
-    repeat_at: Optional[time] = Body(None),
-    process_after: Optional[timedelta] = Body(None),
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
