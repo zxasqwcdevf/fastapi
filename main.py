183afe0f7c09b3e55af899dd4b070da373441588
zxasqwcdevf <zxasqwcdevf@gmail.com>
2022-05-10 15:49:22 +0900

    commit

--- main.py
+++ main.py
@@ -7,7 +7,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-futures = [...]
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.wait(futures))
