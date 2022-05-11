5592eda22e7f03446614627ad473396dea1014e7
zxasqwcdevf <zxasqwcdevf@gmail.com>
2022-05-11 10:16:25 +0900

    commit

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import asyncio
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = Field(
+        None, title="The description of the item", max_length=300
+    )
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
+    tax: Optional[float] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
